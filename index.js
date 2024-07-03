66776b1a1f55ebdd5908074989e9ac682aa745b6
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-03 18:05:46 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin (record){
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+};
+
+const record = [
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+];
+
+console.log(superbowlWin(record));
